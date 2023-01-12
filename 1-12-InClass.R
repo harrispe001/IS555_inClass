95983c3841f9c5af82cfe83ab6847e86d57eed76
Spencer Harrison <spencerharrison99@gmail.com>
2023-01-12 15:44:04 -0700

    forgot to save

--- 1-12-InClass.R
+++ 1-12-InClass.R
@@ -1,2 +1,18 @@
-library(nycflights13)
-flights
+library(tidyverse)
+
+#(starwars tibble is always present when the tidyverse is loaded)
+starwars
+see_it <- starwars
+#filtering
+
+starwars_new <- starwars %>%
+  filter(height > 100,
+         sex == 'female') %>% 
+  select(name, height, mass, species, films) %>% 
+  # print(n=50)
+  # View()
+  arrange(desc(mass) ) %>% 
+  # arrange(mass)
+  mutate(
+    weight_lbs = mass * 2.204
+  )
